ebe8e6a804d6fb6b0d745ca5dbd6c051a2330640
Héctor Martínez Alonso <hector@hectoridris.local>
2015-12-11 10:08:17 +0100

    refactoring conllu conversion

--- conllu_to_conll.py
+++ conllu_to_conll.py
@@ -8,7 +8,7 @@ from lib.conll import CoNLLReader
 
 
 parser = argparse.ArgumentParser(description="""Convert conllu to conll format""")
-parser.add_argument('input', help="conllu file", type=Path)
+parser.add_argument('input', help="conllu file")
 parser.add_argument('output', help="target file", type=Path)
 parser.add_argument('--keep_fused_forms', help="By default removes fused tokens", default=False, action="store_true")
 parser.add_argument('--lang', help="specify a language [de, en, it, fr, sv]", default=None, required=True)
@@ -20,6 +20,8 @@ if sys.version_info < (3,0):
 
 POSRANKPRECEDENCEDICT = defaultdict(list)
 POSRANKPRECEDENCEDICT["none"] = []
+POSRANKPRECEDENCEDICT["default"] = "VERB AUX PRON PROPN NOUN ADJ INT ADV ADP DET NUM PART SCONJ CONJ".split(" ")
+
 POSRANKPRECEDENCEDICT["bg"] = []
 POSRANKPRECEDENCEDICT["da"] = []
 POSRANKPRECEDENCEDICT["de"] = "PROPN ADP DET ".split(" ")
@@ -30,8 +32,11 @@ POSRANKPRECEDENCEDICT["fr"] = "VERB AUX PRON NOUN ADJ ADV ADP DET PART SCONJ CON
 POSRANKPRECEDENCEDICT["it"] = "VERB AUX ADV PRON ADP DET".split(" ")
 POSRANKPRECEDENCEDICT["sv"] = []
 
+
 # Read the treebank sentences
 
 cio = CoNLLReader()
 orig_treebank = cio.read_conll_u(args.input, args.keep_fused_forms, args.lang, POSRANKPRECEDENCEDICT)
+
+
 cio.write_conll_2006(orig_treebank,args.output)
--- lib/conll.py
+++ lib/conll.py
@@ -2,6 +2,29 @@ from collections import defaultdict
 import networkx as nx
 
 
+def parse_id(id_str):
+    if id_str == '_':
+        return None
+    ids = tuple(map(int, id_str.split("-")))
+    if len(ids) == 1:
+        return ids[0]
+    else:
+        return ids
+
+def parse_feats(feats_str):
+    if feats_str == '_':
+        return {}
+    feat_pairs = [pair.split("=") for pair in feats_str.split("|")]
+    return {k: v for k, v in feat_pairs}
+
+def parse_deps(dep_str):
+    if dep_str == '_':
+        return []
+    dep_pairs = [pair.split(":") for pair in dep_str.split("|")]
+    return [(int(pair[0]), pair[1]) for pair in dep_pairs]
+
+
+
 
 class DependencyTree(nx.DiGraph):
     """
@@ -9,6 +32,7 @@ class DependencyTree(nx.DiGraph):
     nodes store information about tokens
     edges store edge related info, e.g. dependency relations
     """
+
     def __init__(self):
         nx.DiGraph.__init__(self)
 
@@ -54,12 +78,18 @@ class DependencyTree(nx.DiGraph):
 
 
 
-CONLL06_COLUMNS = ['id', 'word', 'lemma', 'cpos', 'pos', 'feats', 'head', 'deprel', 'phead', 'pdeprel']
 
 class CoNLLReader(object):
     """
     conll input/output
     """
+
+    "" "Static properties"""
+    CONLL06_COLUMNS = ['id', 'form', 'lemma', 'cpos', 'pos', 'feats', 'head', 'deprel', 'phead', 'pdeprel']
+    CONLL_U_COLUMNS = [('id', parse_id), ('form', str), ('lemma', str), ('cpostag', str),
+                   ('postag', str), ('feats', str), ('head', parse_id), ('deprel', str),
+                   ('deps', parse_deps), ('misc', str)]
+
     def __init__(self):
         pass
 
@@ -70,7 +100,7 @@ class CoNLLReader(object):
         for conll_line in conll_path.open():
             parts = conll_line.strip().split()
             if len(parts) in (8, 10):
-                token = dict(zip(CONLL06_COLUMNS, parts))
+                token = dict(zip(self.CONLL06_COLUMNS, parts))
                 p = ParsedToken(token)
                 sent.add_node(p.id, p.to_dict())
                 # Remove head information from the node properties to avoid confusion
@@ -85,7 +115,6 @@ class CoNLLReader(object):
         if len(sent):
             yield sent
 
-
     def write_conll_2006(self, list_of_graphs, conll_path):
         with conll_path.open('w') as out:
             for sent_i, sent in enumerate(list_of_graphs):
@@ -98,15 +127,85 @@ class CoNLLReader(object):
                     # print(head_i, token_i)
                     token_dict['deprel'] = sent[head_i][token_i]['deprel']
                     token_dict['id'] = token_i
-                    row = [str(token_dict.get(col, '_')) for col in CONLL06_COLUMNS]
+                    row = [str(token_dict.get(col, '_')) for col in self.CONLL06_COLUMNS]
                     print(u"\t".join(row), file=out)
-
+                    #TODO the write method ONLY has to write, not to modify the trees. We will this in _filter_sentence_content
+                    if token_i in sent.graph["multi_tokens"]:
+                        currentmulti = sent.graph["multi_tokens"][token_i]
+                        currentmulti["id"]=str(currentmulti["id"][0])+"-"+str(currentmulti["id"][1])
+                        currentmulti["feats"]="_"
+                        currentmulti["head"]="_"
+                        rowmulti = [str(currentmulti.get(col, '_')) for col in self.CONLL06_COLUMNS]
+                        print(u"\t".join(rowmulti),file=out)
+            print(u"\t".join(row),file=out)
             # emtpy line afterwards
             print(u"", file=out)
 
+    def _remove_fields(self,sent,fields):
+        #TODO remove fx "form" and "label"
+
+        return sent
+
+    def _keep_label_prefixes(self,sent):
+        #TODO nmod:tmod --> nmod
+
+        return sent
+
+    def _keep_fused_form(self,sent):
+        for fusedform in sent.graph["multi_tokens"]:
+                print(sent.graph["multi_tokens"][fusedform])
+        return sent
 
 
-    def read_conll_u(self, conll_path, keepFusedForm=False, lang=None, posPreferenceDict=None):
+    def _filter_sentence_content(self,sent_deptree,keepFusedForm=False, lang=None, posPreferenceDict=None):
+        if keepFusedForm:
+            sent = self._keep_fused_form(sent_deptree)
+        return sent_deptree
+
+    def read_conll_u(self,filename,keepFusedForm=False, lang=None, posPreferenceDict=None):
+        sentences = []
+        sent = DependencyTree()
+        multi_tokens = {}
+
+        for line_no, line in enumerate(open(filename).readlines()):
+            line = line.strip("\n")
+            if not line:
+                # Add extra properties to ROOT node if exists
+                if 0 in sent:
+                    for key in ('form', 'lemma', 'cpostag', 'postag'):
+                        sent.node[0][key] = 'ROOT'
+
+                # Handle multi-tokens
+                sent.graph['multi_tokens'] = multi_tokens
+                multi_tokens = {}
+                sentences.append(sent)
+                sent = DependencyTree()
+            elif line.startswith("#"):
+                if 'comment' not in sent.graph:
+                    sent.graph['comment'] = [line]
+                else:
+                    sent.graph['comment'].append(line)
+            else:
+                parts = line.split("\t")
+                if len(parts) != len(self.CONLL_U_COLUMNS):
+                    error_msg = 'Invalid number of columns in line {} (found {}, expected {})'.format(line_no, len(parts), len(CONLL_U_COLUMNS))
+                    raise Exception(error_msg)
+
+                token_dict = {key: conv_fn(val) for (key, conv_fn), val in zip(self.CONLL_U_COLUMNS, parts)}
+                if isinstance(token_dict['id'], int):
+                    sent.add_edge(token_dict['head'], token_dict['id'], deprel=token_dict['deprel'])
+                    sent.node[token_dict['id']].update({k: v for (k, v) in token_dict.items()
+                                                        if k not in ('head', 'id', 'deprel', 'deps')})
+                    for head, deprel in token_dict['deps']:
+                        sent.add_edge(head, token_dict['id'], deprel=deprel, secondary=True)
+                else:
+                    #print(token_dict['id'])
+                    first_token_id = int(token_dict['id'][0])
+                    multi_tokens[first_token_id] = token_dict
+        return [self._filter_sentence_content(s,keepFusedForm,lang,posPreferenceDict) for s in sentences]
+
+
+    def read_conll_u_old(self, conll_path, keepFusedForm=False, lang=None, posPreferenceDict=None):
         """
         default is like Dan Zeman's conllu_to_conll.pl:   (https://github.com/UniversalDependencies/tools/blob/master/conllu_to_conllx.pl)
         - default: remove comments and remove fused forms
@@ -125,7 +224,7 @@ class CoNLLReader(object):
             if len(parts)>10:
                 parts = parts[:11] #ignore additional columns (secondary edges etc)
             if len(parts) in (8, 10):
-                token = dict(zip(CONLL06_COLUMNS, parts))
+                token = dict(zip(self.CONLL06_COLUMNS, parts))
                 ptoken = ParsedToken(token)
                 sentence.append(ptoken)
             elif len(parts) == 0:
@@ -166,6 +265,8 @@ class CoNLLReader(object):
                         endpos = refTokens[token.end-1].cpos
                         ## check which has preference
 
+                        print(span_makes_subtree(instance,token.start,token.end))
+
                         try:
                             #print(startpos, endpos, token.start, token.end)
                             if posPreferenceDict[lang].index(startpos) < posPreferenceDict[lang].index(endpos):
@@ -174,6 +275,7 @@ class CoNLLReader(object):
                                 head_syntax_t = refTokens[token.end-1]
                         except:
                             print("Except",token)
+                            head_syntax_t= refTokens[token.start-1] #Patch
 
                         # new form and lemma is fused form
                         head_syntax_t.form = token.form
@@ -203,13 +305,33 @@ class CoNLLReader(object):
                         elif t.head in skippedindices:
                             t.head = keptindices.index(skippedindices[t.head])+1
                         else:
-                            t.head = keptindices.index(t.head)+1
+                            try:
+                                t.head = keptindices.index(t.head)+1
+                            except:
+                                print(t, token, keptindices)
                         sent_graph.add_node(t.id,t.to_dict())
                         sent_graph.add_edge(t.head, t.id, deprel=t.deprel)
 
             trees.append(sent_graph)
 
         return trees
+
+
+def span_makes_subtree(sent_graph, initidx, endidx):
+    G = nx.Graph()
+    try:
+        span_nodes = list(range(initidx,endidx+1))
+        span_words = [sent_graph.node[x]["form"] for x in span_nodes]
+        print(sent_graph,span_nodes)
+    except:
+        print(sent_graph.nodes())
+
+    G.add_nodes_from(span_nodes)
+    for h,d in sent_graph.edges():
+        if h in span_nodes and d in span_nodes:
+            G.add_edge(h,d)
+    return span_words,span_nodes,nx.is_tree(G)
+
 
 class ParsedToken(object):
     """
